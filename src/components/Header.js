a30e968a0984f219cfd399e07334bf866c7151af
Ramkumar Dehariya <ramkumardehariya26@gmail.com>
2024-04-14 23:41:35 +0530

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,6 +14,7 @@ import {
 } from "@mui/material/styles";
 import { useNavigate } from "react-router-dom";
 import { CryptoState } from "../CryptoContext";
+import AuthModal from "./Authentication/AuthModal";
 
 const useStyles = makeStyles((theme) => ({
   title: {
@@ -64,7 +65,7 @@ function Header() {
               <MenuItem value={"USD"}>USD</MenuItem>
               <MenuItem value={"INR"}>INR</MenuItem>
             </Select>
-            
+            <AuthModal/>
           </Toolbar>
         </Container>
       </AppBar>
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+ 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBO9CX7ZdWaPPIyJbACg57v_mIBFtFiKvQ",
+  authDomain: "crypto-tracker-c4db9.firebaseapp.com",
+  projectId: "crypto-tracker-c4db9",
+  storageBucket: "crypto-tracker-c4db9.appspot.com",
+  messagingSenderId: "3562644488",
+  appId: "1:3562644488:web:2361e3d49f034793f8ddbd",
+  measurementId: "G-DSWXQK4PZ6"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export default firebaseConfig;
